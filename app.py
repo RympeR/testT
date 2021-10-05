c3b778a2aa285f3b89f924f853724df4b77131c6
George <42513185+RympeR@users.noreply.github.com>
2021-10-05 11:16:59 +0300

    added simple route

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ def hello_world():
     return 'Hello, Docker!'
 
 
+@app.route('/test-route')
+def hello_world():
+    return "I'd added test route"
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
